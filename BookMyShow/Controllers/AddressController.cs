c95ade9b72ef513097fd8adeeb9c76dc8ae7fe3d
vikaschercadu <vikaschercadu.222@gmail.com>
2021-04-30 13:21:47 +0530

    added controllers

---
+++ BookMyShow/Controllers/AddressController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookMyShow.Controllers
+{
+    public class AddressController : ApiController
+    {
+        // GET: api/Address
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Address/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Address
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Address/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Address/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BookMyShow/Controllers/CityController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookMyShow.Controllers
+{
+    public class CityController : ApiController
+    {
+        // GET: api/City
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/City/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/City
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/City/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/City/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
